138b20b6ac2829646f35f285d2dd15e766c2a1a4
VirginiaBarrios <virginia.aye.bar@gmail.com>
2023-04-29 21:35:58 -0300

    commit 5

--- Blog/urls.py
+++ Blog/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import * 
 
 urlpatterns = [
-    path('Blog1/', inicio, name="inicio"),
-    path('Blog2/', contacto, name="contacto"),
-    path('Blog3/', posteos, name="posteos"),
-    path('Blog4/', sobreMi, name="SobreMi"),
+    path('inicio/', inicio, name="Inicio"),
+    path('contacto/', contacto, name="Contacto"),
+    path('posteos/', posteos, name="Posteos"),
+    path('sobreMi/', sobreMi, name="SobreMi"),
     
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def inicio(request):
+def inicio(request): 
     return render(request, "inicio.html")
 
 def contacto(request):
